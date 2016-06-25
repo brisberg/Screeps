e81bb1d0ca9457308d1b608ebb31dfca7c125747
brisberg <brandon.risberg@gmail.com>
2016-06-25 01:57:02 -0700

    added Grunt option to specify screep branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
             options: {
                 email: '<%= pkg.email %>',
                 password: '<%= pkg.password %>',
-                branch: 'test1',
+                branch: grunt.option('branch') || 'default',
                 ptr: false
             },
             dist: {
