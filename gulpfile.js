b12aaa864a0a76ef737d80ba45efb40c0883b0c8
Brandon Risberg <brandon@betterworks.com>
2016-08-09 10:59:55 -0700

    gulp commandline args

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
 var screeps = require('gulp-screeps');
 var rename = require('gulp-rename');
+
 var credentials = require('./config/screeps-creds.js');
+var minimist = require('minimist');
+
+var knownOptions = {
+    string: 'branch',
+    boolean: 'ptr',
+    default: credentials
+};
+
+credentials = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('screeps', function() {
   gulp.src('./src/**/*.js', { base: './src' })
